47a028b832930df0d40069b32153f0fa64e5a081
joao_silva <joao.silva@finnet.com.br>
2020-04-27 17:14:36 -0300

    1.0.2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect, url_for, send_file
 import json
 
 app = Flask(__name__)
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/files')
+def file():
+    return redirect(url_for('static', filename='/files/curriculojoao.pdf'))
 
 
 if __name__ == '__main__':
